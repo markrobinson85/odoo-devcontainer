eb7ec840c01eeb3b631654d00c5140e96691e29f
Mark Robinson <mark.robinson@canzonia.com>
2023-12-30 13:28:16 -0800

    [WIP] escape module_dir

--- .devcontainer/pre-setup.sh
+++ .devcontainer/pre-setup.sh
@@ -148,7 +148,7 @@ cat >> /workspace/.idea/odoo-workspace.iml <<EOL
 <?xml version="1.0" encoding="UTF-8"?>
 <module type="PYTHON_MODULE" version="4">
   <component name="NewModuleRootManager">
-    <content url="file://$MODULE_DIR$">
+    <content url="file://\$MODULE_DIR\$">
     </content>
     <orderEntry type="inheritedJdk" />
     <orderEntry type="sourceFolder" forTests="false" />
