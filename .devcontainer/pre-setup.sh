4c4114583be74540047710c5ef342824d2065313
Mark Robinson <mark.robinson@canzonia.com>
2023-12-30 14:07:08 -0800

    [WIP] create modules.xml file.

--- .devcontainer/pre-setup.sh
+++ .devcontainer/pre-setup.sh
@@ -159,6 +159,20 @@ cat >> /workspace/.idea/workspace.iml <<EOL
 EOL
 fi
 
+if [ ! -f "/workspace/.idea/modules.xml" ]; then
+## Create modules.xml file if it does not exist.
+cat >> /workspace/.idea/modules.xml <<EOL
+<?xml version="1.0" encoding="UTF-8"?>
+<project version="4">
+  <component name="ProjectModuleManager">
+    <modules>
+      <module fileurl="file://$PROJECT_DIR$/.idea/workspace.iml" filepath="$PROJECT_DIR$/.idea/workspace.iml" />
+    </modules>
+  </component>
+</project>
+EOL
+fi
+
 iml_file=$(find .idea -name "*.iml")
 module_name=$(basename "$iml_file" .iml)
 echo "Project name: $module_name"
